2ae32ef06682efd8e0d5cc9ef0e5b151bce267fd
Eslam Waled <62679079+eslamwaled@users.noreply.github.com>
2024-06-06 03:19:48 +0300

    Modified

--- Project/Profiles/HotelProfile.cs
+++ Project/Profiles/HotelProfile.cs
@@ -5,6 +5,7 @@ public class HotelProfile : Profile
 {
     public HotelProfile()
     {
+        CreateMap<Hotel, HotelDto>();
         CreateMap<HotelCreateDto, Hotel>();
         CreateMap<HotelUpdateDto, Hotel>()
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
