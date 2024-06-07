b431c30f4ea39dad1997eba07131c6b435bc5d86
Eslam Waled <62679079+eslamwaled@users.noreply.github.com>
2024-06-07 21:28:10 +0300

    Last update

--- Project/Services/IAddressRepository.cs
+++ Project/Services/IAddressRepository.cs
@@ -3,10 +3,10 @@ namespace HotelBookingSystem.Services
     public interface IAddressRepository
     {
         public Task<bool> AddressExistsAsync(int id);
-        //public Task<IEnumerable<Address>> GetAllAddressesAsync();
-        //public Task<Address?> GetAddressByIdAsync(int addressId);
+        public Task<IEnumerable<Address>> GetAllAddressesAsync();
+        public Task<Address?> GetAddressByIdAsync(int addressId);
         public Task CreateAddressAsync(Address address);
         public Task UpdateAddressAsync(Address address);
-        //public Task DeleteAddressAsync(int addressId);
+        public Task DeleteAddressAsync(int addressId);
     }
 }
